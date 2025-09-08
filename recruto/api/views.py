26ff8fedcb308aeeefe3c6747226f84202f4e6fd
uselesspart <uselesspartinfo@gmail.com>
2025-09-08 18:46:13 +0300

    defaults

--- recruto/api/views.py
+++ recruto/api/views.py
@@ -5,8 +5,8 @@ def hello(request):
     if request.method != "GET":
         return HttpResponse("Wrong method")
     
-    name = request.GET.get('name')
-    message = request.GET.get('message')
+    name = request.GET.get('name', '')
+    message = request.GET.get('message', '')
     
     html_content = f"<h1> Hello {name}! {message}!</h1>"
     return HttpResponse(html_content)

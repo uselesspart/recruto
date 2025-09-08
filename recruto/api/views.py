9080765e8c36d905fc665cc63da727ab37b118f1
uselesspart <uselesspartinfo@gmail.com>
2025-09-08 18:26:51 +0300

    Init

---
+++ recruto/api/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+def hello(request):
+    if request.method != "GET":
+        return HttpResponse("Wrong method")
+    
+    name = request.GET.get('name')
+    message = request.GET.get('message')
+    
+    html_content = f"<h1> Hello {name}! {message}!</h1>"
+    return HttpResponse(html_content)

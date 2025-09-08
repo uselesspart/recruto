8ea3bd80c8caaf947b282edb8bc3d5caddf2649d
uselesspart <uselesspartinfo@gmail.com>
2025-09-08 18:33:22 +0300

    hosts

--- recruto/recruto/settings.py
+++ recruto/recruto/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_4=ovdfpwhmi-!=pq)s9ao20ikaxgf^=r8+t+#51mtr69w!bse'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['83.166.247.45']
 
 
 # Application definition
